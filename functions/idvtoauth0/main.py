78f4de9228bf9d60d1ce0fa10fe0c17cd0dc23d7
Guillaume Destuynder (kang) <kang+mozilla@insecure.ws>
2017-06-06 16:34:29 -0700

    mv auth0.py=>authzero.py so that it does not collide with any potential auth0 upstream libs (and matches the class name)

--- functions/idvtoauth0/main.py
+++ functions/idvtoauth0/main.py
@@ -1,5 +1,5 @@
 """ This is the body of the lambda function """
-import auth0
+import authzero
 import boto3
 import credstash
 import json
@@ -19,8 +19,8 @@ def handle(event, context):
     logger = logging.getLogger('cis-idvtoauth0')
     logger.info("Stream Processor initialized.")
 
-    # New up the config object for CISAuth0
-    config = auth0.DotDict(dict())
+    # New up the config object for CISAuthZero
+    config = authzero.DotDict(dict())
     config.client_id = credstash.getSecret(
         name="cis.client_id",
         context={'app': 'cis', 'environment': 'dev'},
@@ -39,7 +39,7 @@ def handle(event, context):
         region="us-east-1"
     )
 
-    client = auth0.CISAuthZero(config)
+    client = authzero.CISAuthZero(config)
     access_token = client.get_access_token()
 
     for record in event['Records']:
