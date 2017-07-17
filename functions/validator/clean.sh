e147d149faaf6bc5358366d06757b6963733403c
andrewkrug <andrewkrug@gmail.com>
2017-07-17 15:49:01 -0700

    update clean hook to exclude tests

--- functions/validator/clean.sh
+++ functions/validator/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ### Cleanup virtualenv post-deploy
-find . -d -not -name ".." -not -name "." -not -name "*.json" -not -name "clean.sh" -not -name "main.py" -not -name "function.*" -not -name "utils.py" -not -name "requirements.txt" | xargs rm -fr &> /dev/null
-find . -type f -name "*.py" -name "*.so" -not -name "*.json" -not -name "clean.sh" -not -name "main.py" -not -name "utils.py" -not -name "function.*" -not -name "requirements.txt" | xargs rm -fr &> /dev/null
-find . -type f -name "*.py" -name "*.so" -not -name "*.json" -not -name "clean.sh" -not -name "utils.py" -not -name "function.*" -not -name "requirements.txt" | xargs rm -fr &> /dev/null
+find . -d -not -name ".." -not -name "." -not -name "tests" -not -name "data" -not -name "*.json" -not -name "__init__.py" -not -name "test_validator.py" -not -name "clean.sh" -not -name "main.py" -not -name "function.*" -not -name "utils.py" -not -name "requirements.txt" | xargs rm -fr &> /dev/null
+find . -type f -name "*.py" -name "*.so" -not -name "__init__.py" -not -name "test_validator.py" -not -name "profile-*.json" -not -name "fixtures.json" -not -name "*.json" -not -name "clean.sh" -not -name "main.py" -not -name "utils.py" -not -name "function.*" -not -name "requirements.txt" | xargs rm -fr &> /dev/null
+#find . -type f -name "*.py" -name "*.so" -not -name "*.json" -not -name "clean.sh" -not -name "utils.py" -not -name "function.*" -not -name "requirements.txt" | xargs rm -fr &> /dev/null
