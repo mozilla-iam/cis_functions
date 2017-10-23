10c3f3ec57e1b55524914876fdee86687677fc77
John Giannelos <johngiannelos@gmail.com>
2017-10-23 14:50:33 +0300

    Allow publishers to delete groups.
    * Block non whitelisted users only in case groups exist

--- functions/idvtoauth0/main.py
+++ functions/idvtoauth0/main.py
@@ -88,7 +88,7 @@ def handle(event, context):
             # Profile whitelisting. This allows to select which user profiles are
             # to be integrated using CIS, mainly for transitioning purposes.
             # See also: https://mozillians.org/en-US/group/cis_whitelist
-            if 'mozilliansorg_cis_whitelist' not in profile['groups']:
+            if profile['groups'] and 'mozilliansorg_cis_whitelist' not in profile['groups']:
                 continue
 
             # XXX Force-integrate LDAP groups as these are synchronized
